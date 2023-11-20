8292fa9474916b2d6eff71c773c887b9010be7b1
DauletKalesh <dauletkalesov@gmail.com>
2023-11-20 22:59:37 +0600

    development

--- project.py
+++ project.py
@@ -148,7 +148,6 @@ if __name__=="__main__":
         function = lambda x: x[0]**2 + x[1]**2 + 2*x[1] + x[2]**2 + (x[3] + 4)**2 + (x[4] - 1)**2 + (x[5] - 3)**2,
         x_n = np.zeros(6)
     )
-    8
 
     # verification
 
